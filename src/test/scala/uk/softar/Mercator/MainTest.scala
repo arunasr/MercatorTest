775b52ed9ca343087bcab8e50405479ec6ae601c
Arūnas Rukšnaitis <arunas.ruksnaitis@softar.uk>
2022-05-06 21:07:15 +0100

    Unit tests added

--- src/test/scala/uk/softar/Mercator/MainTest.scala
+++ src/test/scala/uk/softar/Mercator/MainTest.scala
@@ -4,7 +4,26 @@ import org.scalatest.flatspec._
 import org.scalatest.matchers.should
 
 class MainTest extends AnyFlatSpec with should.Matchers {
-  "Main object" should "exist" in {
-    Main.getClass should not be null
+  private val priceProvider = new PriceProvider {
+    private val prices = Map("Apple" → 60, "Orange" → 25)
+
+    override def getPrice(product: Product): Price =
+      prices(product)
+  }
+
+  "Prices" should "be correct" in {
+    priceProvider getPrice "Apple" shouldEqual 60
+    priceProvider getPrice "Orange" shouldEqual 25
+  }
+
+  "Checkout" should "return correct totals" in {
+    val checkout = Checkout(priceProvider)
+    case class TestBasket(val products: Seq[PriceProvider#Product]) extends BasketInterface {
+      override def getItems: Seq[Product] = products
+    }
+    checkout getTotal TestBasket(Seq.empty) shouldEqual 0
+    checkout getTotal TestBasket(Seq("Apple")) shouldEqual 60
+    checkout getTotal TestBasket(Seq("Orange")) shouldEqual 25
+    checkout getTotal TestBasket("Apple,Apple,Orange,Apple".split(',')) shouldEqual 205
   }
 }
